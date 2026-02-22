eddd175fc2e4e3d1f3b059c8412ece8960cd4e1b
borzcodes <realmahmoudcodes@gmail.com>
2026-02-22 04:15:49 +0000

    'implement the canvas'

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import useMacbookStore from "../store/index.js";
 import clsx from "clsx";
+import {Canvas} from "@react-three/fiber";
+import {Box, OrbitControls} from "@react-three/drei";
 
 const ProductViewer = () => {
     const {color,scale,setColor,setScale} = useMacbookStore();
@@ -28,15 +30,28 @@ const ProductViewer = () => {
                     </div>
 
                     <div className='size-control'>
-                        <div><p>14"</p></div>
-                        <div><p>16"</p></div>
+                        <div
+                            onClick={()=> setScale(0.06)}
+                            className={clsx(scale === 0.06 ? 'bg-white text-black' : 'bg-transparent text-white')}
+                        >
+                            <p>14"</p>
+                        </div>
+
+                        <div
+                            onClick={()=> setScale(0.08)}
+                            className={clsx(scale === 0.08 ? 'bg-white text-black' : 'bg-transparent text-white')}
+                        >
+                            <p>16"</p>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <p className='text-white text-4xl'>
-                Render Canvas
-            </p>
+           <Canvas id='canvas' camera={{position:[0,2,5], fov:50, near:0.1, far:100}}>
+               <Box position={[0, 0, 0]} scale={10 * scale} material-color={color} />
+
+               <OrbitControls enableZoom={false}/>
+           </Canvas>
         </section>
     )
 }
