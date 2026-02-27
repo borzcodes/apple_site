11941adf0ab0eff12eadfe6cf1235836b9cb96da
borzcodes <realmahmoudcodes@gmail.com>
2026-02-27 00:36:00 +0000

    'replacing the last product view code'

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -3,43 +3,40 @@ import useMacbookStore from "../store/index.js";
 import clsx from "clsx";
 import {Canvas} from "@react-three/fiber";
 import {Box, OrbitControls} from "@react-three/drei";
-import MacbookModel14 from "./models/Macbook-14.jsx";
 
 const ProductViewer = () => {
-    const {color,scale,setColor,setScale} = useMacbookStore();
+    const {color, scale,setColor,setScale } = useMacbookStore();
 
     return (
-        <section id='product-viewer'>
+        <section id="product-viewer">
             <h2>Take a closer look.</h2>
 
-            <div className='controls'>
-                <p className='info'>
-                    MacBookPro 16" in {color}
-                </p>
+            <div className="controls">
+                <p className="info">MacbookPro 16" in Space {color} </p>
 
-                <div className='flex-center gap-5 mt-5'>
-                    <div className='color-control'>
+                <div className="flex-center gap-5 mt-5">
+                    <div className="color-control">
                         <div
-                            onClick={()=> setColor('#abd5bd')}
-                            className={clsx('bg-neutral-300', color === '#abd5bd' && 'active')}
+                            onClick={() => setColor('#adb5bd')}
+                            className={clsx('bg-neutral-300', color === '#adb5bd' && 'active')}
                         />
 
                         <div
-                            onClick={()=> setColor('#2e2c2e')}
+                            onClick={() => setColor('#2e2c2e')}
                             className={clsx('bg-neutral-900', color === '#2e2c2e' && 'active')}
                         />
                     </div>
 
-                    <div className='size-control'>
+                    <div className="size-control">
                         <div
-                            onClick={()=> setScale(0.06)}
+                            onClick={() => setScale(0.06)}
                             className={clsx(scale === 0.06 ? 'bg-white text-black' : 'bg-transparent text-white')}
                         >
                             <p>14"</p>
                         </div>
 
                         <div
-                            onClick={()=> setScale(0.08)}
+                            onClick={() => setScale(0.08)}
                             className={clsx(scale === 0.08 ? 'bg-white text-black' : 'bg-transparent text-white')}
                         >
                             <p>16"</p>
@@ -48,13 +45,11 @@ const ProductViewer = () => {
                 </div>
             </div>
 
-           <Canvas id='canvas' camera={{position:[0,2,5], fov:50, near:0.1, far:100}}>
-               <ambientLight intensity={1} />
+            <Canvas id="canvas" camera={{position:[0, 2, 5], fov:50, near:0.1, far:100}}>
+                <Box position={[0, 0, 0]} scale={10 * scale} material-color={color} />
 
-               <MacbookModel14 scale={0.06} position={[0, 0, 0]} />
-
-               <OrbitControls enableZoom={false}/>
-           </Canvas>
+                <OrbitControls enableZoom={false}/>
+            </Canvas>
         </section>
     )
 }
