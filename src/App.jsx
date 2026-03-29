743cb9a4f596a7ebf5e45ca6f104a43927638484
borzcodes <realmahmoudcodes@gmail.com>
2026-03-29 20:20:11 +0100

    'implementing model switch'

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import NavBar from "./components/NavBar.jsx";
 import Hero from "./components/Hero.jsx";
 import ProductViewer from "./components/ProductViewer.jsx";
+import {ScrollTrigger} from "gsap/all";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
     return (
         <main>
             <NavBar />
-            <Hero/>
-            <ProductViewer/>
+            <Hero />
+            <ProductViewer />
         </main>
     )
 }
